304dacd783b99ce5d8210b87a5d34edb76691610
DanNguyen0117 <91091870+DanNguyen0117@users.noreply.github.com>
2023-03-21 18:18:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Automatic Attendence Form
+# First created on 1/26/2021 on ChromeDriver 86, program worked as of 2/24/2021 using ChromeDriver 86, current code 3/31/2021 on ChromeDriver 88 with some cleanup code to make it more readable
+# Description: Program opens Chrome, signs in to Google and RapidIdentity account (my school used both), and autofills out google form sheet that had {firstname, lastname, studentID, classperiod, buttonyesIampresent}. 
+# This program is definitely outdated and not really mass-producible, it's using chromedriver88 and solves a niche problem I had in my senior year of high school which can't really translate unless you massively change the code.
+
+# So how this happened was when our school was starting under COVID, these Google attendence forms were their way of tracking attendence. It was a tedious process since you had to click the link, type in the same information, and do it again for however many classes you had.
+# Basically I made this because I got tired of spending 3 minutes going through the attendence forms, so I made this program to do it in 30 seconds.
+# Took me like a month to finish it tho. Uses Python Selenium, Python 2.7, Chromedriver 88. 
+# Python 3 does not work for this, only 2.7
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,6 +15,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # DO NOT USE VPN WHEN USING PROGRAM (google hates vpns and will break program)
 
+#delay between filling in info, made this because program could fill in information before chrome could load in the url which would break the program
 def sleep():
     time.sleep(1.3)
 
@@ -13,6 +24,7 @@ browser = webdriver.Chrome('/Users/dan/Downloads/chromedriver_88')
 try:
     def google_sign_in():
         # specific gmail portal used for automation
+        # main reason why this project took forever was because Google wasn't letting me sign in through automation fill-in, but this site was able to do so
         gmail_portal = 'https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=https%3A%2F' \
                        '%2Fdevelopers.google.com%2Foauthplayground&prompt=consent&response_type=code&client_id=407408718192' \
                        '.apps.googleusercontent.com&scope=email&access_type=offline&flowName=GeneralOAuthFlow'
@@ -23,7 +35,7 @@ try:
 
         # gmail username sign in
         my_email = browser.find_element_by_xpath('//*[@id="identifierId"]')
-        my_email.send_keys('s21600529')
+        my_email.send_keys('xxxxx')
         my_email.send_keys('@stu.tempeunion.org')
         sleep()
         next_key = browser.find_element_by_class_name('VfPpkd-RLmnJb')
@@ -32,12 +44,12 @@ try:
 
         # RapidIdentity username sign in
         rp_username = browser.find_element_by_xpath('//*[@id="identification"]')
-        rp_username.send_keys('s21600529')
+        rp_username.send_keys('xxStudentIDxx')
         sleep()
 
         # RapidIdentity password sign in
         rp_password = browser.find_element_by_xpath('//*[@id="ember489"]')
-        rp_password.send_keys('Huud@no3Huud@no3')
+        rp_password.send_keys('xxMyPasswordxx') # yes I hardcoded my password but for a single use project it got the job done 
         sleep()
 
         # RapidIdentity go button click
@@ -53,28 +65,15 @@ try:
         tab_key.perform()
         enter_key.perform()
 
-        # # gmail password sign in
-        # my_password = browser.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
-        # my_password.send_keys('Huud@no3Huud@no3')
-        # sleep()
-        # s_next_key = browser.find_element_by_class_name('VfPpkd-RLmnJb')
-        # s_next_key.click()
-
         time.sleep(5)
 
 
-    # storing values in the list
-    firstName = 'Dan'
-    lastName = 'Nguyen'
-    studentID = '21600529'
-    test_urls = ['https://docs.google.com/forms/d/e/1FAIpQLScplbnlSpxms7HNJJ_0V3o01jbkoyz9LX4xRyydibARRrRQ0A'
-                 '/viewform?vc=0&c=0&w=1&flr=0 ', 'https://docs.google.com/forms/d/e'
-                 '/1FAIpQLSc3a47akkLSAlvcKa2CVDQdE7zhCJHSze2bHyvWvB3XAp954w'
-                 '/viewform?vc=0&c=0&w=1&flr=0 ', 'https://docs.google.com/forms/d/e'
-                 '/1FAIpQLSeC3Jy2NklYxdgTVr60TLZWXUJjnr3Z1lRm6dUM4HeXuzERdw/viewform',
-                 'https://docs.google.com/forms/d/e'
-                 '/1FAIpQLSfIOE1iucr5VdJA8cYmi6am5MrcWVUW5WrL2IqeM6XoDFntGw/viewform']
+    # storing values in the list. putting in placeholders but this was actually my first name, last name, etc
+    firstName = 'xxFirstNamexx'
+    lastName = 'xxLastNamexx'
+    studentID = 'xxStudentIDxx'
 
+    # the url of the google forms I had to fill out per class
     period_1 = 'https://docs.google.com/forms/d/e/1FAIpQLSdfEfcu9RveVqNcNySRgMCljKzyXXSsmgzR7Pw_xJ8p5zoI9w/viewform'
     period_2 = 'https://docs.google.com/forms/d/e/1FAIpQLSfqTPM4tCCYxPv4Hul0242p4_YHMeORwJbxOSi1mlekLOA5jg/viewform'
     period_3 = 'https://docs.google.com/forms/d/e/1FAIpQLScwG2Dj_lrPUFxqhyYP7guLr3tgkbjkLc6Hzwc17afb_Ox2oA/viewform'
@@ -84,15 +83,20 @@ try:
 
     school_urls = [period_1, period_2, period_3, period_4, period_6, period_7]
 
-    # run google sign in
+    # run google sign in to sign into accounts to access school attendence forms
     google_sign_in()
 
     period = 1
+    # goes through each class form url
     for url in school_urls:
         browser.get(url)
         sleep()
 
-        # finds elements for google form
+        # basically every form had a firstname, lastname, and studentID as textfields
+        # radiobuttons for classperiod and a button that said that you were attending class
+        # and finally there's a submit button
+        
+        # finds elements for google form (found class name using inspect and narrowing down the name)
         textboxes = browser.find_elements_by_class_name('quantumWizTextinputPaperinputInput')
         radioButtons = browser.find_elements_by_class_name('docssharedWizToggleLabeledLabelWrapper')
         submitButton = browser.find_element_by_class_name('appsMaterialWizButtonPaperbuttonContent')
@@ -105,15 +109,13 @@ try:
         radioButtons[11].click()
 
         submitButton.click()
-        
-        # alert_obj = browser.switch_to.alert
-        # alert_obj.accept()
 
         period += 1
 
-        # skips from period 4 to period 6
+        # skips from period 4 to period 6 because period 5 was my lunch
         if period == 5:
             period += 1
+            
     print('Program successfully run')
 
 finally:
